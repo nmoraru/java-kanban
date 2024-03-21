92d278ab3cc3036ab946ef80fc87b362a68c953e
nomoraru <nomoraru@gmail.com>
2024-03-21 11:43:31 +0300

    Code refactoring InMemoryHistoryManager class.

--- src/taskManager/manager/InMemoryHistoryManager.java
+++ src/taskManager/manager/InMemoryHistoryManager.java
@@ -6,20 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    static final int HISTORY_MAX_SIZE = 10;
+    private static final int HISTORY_MAX_SIZE = 10;
 
     private List<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        history.add(task);
-        if (history.size() > InMemoryHistoryManager.HISTORY_MAX_SIZE) {
-            history.remove(0);
+        if (task != null) {
+            history.add(task);
+            if (history.size() > HISTORY_MAX_SIZE) {
+                history.remove(0);
+            }
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        List<Task> historyResult = new ArrayList<>();
+        historyResult.addAll(history);
+        return historyResult;
     }
 }
