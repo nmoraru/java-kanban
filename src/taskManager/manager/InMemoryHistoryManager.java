68229a9b04431a17b3758ff8674ea8c752de8e24
nomoraru <nomoraru@gmail.com>
2024-04-14 21:45:13 +0300

    Make history infinite

--- src/taskManager/manager/InMemoryHistoryManager.java
+++ src/taskManager/manager/InMemoryHistoryManager.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int HISTORY_MAX_SIZE = 10;
 
     private List<Task> history = new ArrayList<>();
 
@@ -14,9 +13,6 @@ public class InMemoryHistoryManager implements HistoryManager {
     public void add(Task task) {
         if (task != null) {
             history.add(task);
-            if (history.size() > HISTORY_MAX_SIZE) {
-                history.remove(0);
-            }
         }
     }
 
