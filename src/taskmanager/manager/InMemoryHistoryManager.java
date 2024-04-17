e965a134be2714bdbb0f5cd14fbdbe0f468e5658
nomoraru <nomoraru@gmail.com>
2024-04-17 23:01:20 +0300

    Code refactoring

--- src/taskmanager/manager/InMemoryHistoryManager.java
+++ src/taskmanager/manager/InMemoryHistoryManager.java
@@ -75,17 +75,17 @@ public class InMemoryHistoryManager implements HistoryManager {
             }
         }
     }
-}
 
-class Node<Task> {
+    class Node<T> {
 
-    public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+        public T data;
+        public Node<T> next;
+        public Node<T> prev;
 
-    public Node(Task data) {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
     }
 }
